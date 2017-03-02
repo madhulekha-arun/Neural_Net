bef05647e29884f901f4845f19d6e80000642b63
Madhulekha <madhulekha.a@gmail.com>
2017-03-01 20:31:18 -0500

    Neural net on person activity

---
+++ experiment.py
@@ -0,0 +1,60 @@
+from numpy import exp, array, random, dot
+
+
+class NeuralNetwork():
+    def __init__(self):
+        random.seed(1)
+        self.synaptic_weights = 2 * random.random((3, 1)) - 1
+
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in xrange(number_of_training_iterations):
+
+            output = self.think(training_set_inputs)
+
+            error = training_set_outputs - output
+
+            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
+            # This means less confident weights are adjusted more.
+            # This means inputs, which are zero, do not cause changes to the weights.
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            # Adjust the weights.
+            self.synaptic_weights += adjustment
+
+    def think(self, inputs):
+        # Pass inputs through our neural network (our single neuron).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
+
+if __name__ == "__main__":
+
+
+    neural_network = NeuralNetwork()
+
+    print "Random starting synaptic weights: "
+    print neural_network.synaptic_weights
+
+    df = pd.read_csv('A01.txt')
+    m = df.as_matrix()
+    X_t = m[,:-1]
+
+    trainX, testX, trainY, testY = train_test_split(X_t, Y_t, test_size=0.2, random_state=0)
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    # Train the neural network using a training set.
+    # Do it 10,000 times and make small adjustments each time.
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print "New synaptic weights after training: "
+    print neural_network.synaptic_weights
+
+    # Test the neural network with a new situation.
+    print "Considering new situation [1, 0, 0] -> ?: "
+    print neural_network.think(array([1, 0, 0]))
